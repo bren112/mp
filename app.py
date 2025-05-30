9b974d29eb623acff028a59829cf073328b165a4
bren112 <brenomarinho1610@gmail.com>
2025-05-30 13:24:50 -0300

    Teste

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mercadopago
+import os
+
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -86,6 +89,7 @@ def verificar_status(payment_id):
 def listar_pagamentos_aprovados():
     aprovados = [pag for pag in pagamentos_realizados if pag["status"] == "approved"]
     return jsonify(aprovados)
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
